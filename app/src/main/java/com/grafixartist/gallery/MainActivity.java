6b271421fe80368fe80f58f4e2632d4cb1e44557
shanhong cheng <shanhong.cheng@utah.edu>
2017-05-23 10:46:59 -0600

    initial commit

--- app/src/main/java/com/grafixartist/gallery/MainActivity.java
+++ app/src/main/java/com/grafixartist/gallery/MainActivity.java
@@ -1,74 +1,177 @@
 package com.grafixartist.gallery;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.amirarcane.recentimages.RecentImages;
+import com.amirarcane.recentimages.thumbnailOptions.ImageAdapter;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
 
-    GalleryAdapter mAdapter;
-    RecyclerView mRecyclerView;
 
-    ArrayList<ImageModel> data = new ArrayList<>();
+    private static final int TAKE_PICTURE = 0;
+    private static final int SELECT_PHOTO = 1;
 
-    public static String IMGS[] = {
-           "https://images.unsplash.com/photo-1444090542259-0af8fa96557e?q=80&fm=jpg&w=1080&fit=max&s=4b703b77b42e067f949d14581f35019b",
-            "https://images.unsplash.com/photo-1439546743462-802cabef8e97?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1441155472722-d17942a2b76a?q=80&fm=jpg&w=1080&fit=max&s=80cb5dbcf01265bb81c5e8380e4f5cc1",
-            "https://images.unsplash.com/photo-1437651025703-2858c944e3eb?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1431538510849-b719825bf08b?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1434873740857-1bc5653afda8?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1439396087961-98bc12c21176?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1433616174899-f847df236857?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1438480478735-3234e63615bb?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1438027316524-6078d503224b?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300"
-    };
+    private Uri imageUri;
+
+    private ImageView image;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for (int i = 0; i < IMGS.length; i++) {
-
-            ImageModel imageModel = new ImageModel();
-            imageModel.setName("Image " + i);
-            imageModel.setUrl(IMGS[i]);
-            data.add(imageModel);
-
+        //Permissions need to be granted at runtime on Marshmallow
+        if (Build.VERSION.SDK_INT >= 21) {
+            CheckPermissions();
         }
+
+
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.list);
-        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
-        mRecyclerView.setHasFixedSize(true);
+        RecentImages ri = new RecentImages();
+        ImageAdapter adapter = ri.getAdapter(MainActivity.this);
 
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(adapter);
 
-        mAdapter = new GalleryAdapter(MainActivity.this, data);
-        mRecyclerView.setAdapter(mAdapter);
+    }
 
-        mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this,
-                new RecyclerItemClickListener.OnItemClickListener() {
+    //take photo via camera intent
+    public void takePhoto(View view) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        String name = String.valueOf(System.currentTimeMillis() + ".jpg");
+        File photo = new File(String.valueOf(Environment.getExternalStorageDirectory()), name);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+        imageUri = Uri.fromFile(photo);
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
 
-                    @Override
-                    public void onItemClick(View view, int position) {
+    public void CheckPermissions()
+    {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+    }
 
-                        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
-                        intent.putParcelableArrayListExtra("data", data);
-                        intent.putExtra("pos", position);
-                        startActivity(intent);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Thanks!",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    //If user denies Storage Permission, explain why permission is needed and prompt again.
+                    Toast.makeText(this, "Storage access is needed to display images.",
+                            Toast.LENGTH_SHORT).show();
+                    CheckPermissions();
+                }
+                break;
+            default:
+                break;
+        }
+    }
 
-                    }
-                }));
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PICTURE:
+                if (resultCode == Activity.RESULT_OK) {
+                }
+                break;
+            case SELECT_PHOTO:
+                if (resultCode == Activity.RESULT_OK) {
+                    imageUri = data.getData();
+                }
+                break;
+        }
+        if (imageUri != null) {
+            Bitmap bitmap = null;
+            try {
+                Log.d("ImageURI", String.valueOf(imageUri));
+                bitmap = MediaStore.Images.Media.getBitmap(MainActivity.this.getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(image != null) {
+                image.setImageBitmap(bitmap);
+            }
+        }
+    }
 
+    private static int getOrientation(ContentResolver cr, int id) {
+
+        String photoID = String.valueOf(id);
+
+        Cursor cursor = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                new String[] {MediaStore.Images.Media.ORIENTATION}, MediaStore.Images.Media._ID + "=?",
+                new String[] {"" + photoID}, null);
+        int orientation = -1;
+
+        if (cursor.getCount() != 1) {
+            return -1;
+        }
+
+        if (cursor.moveToFirst())
+        {
+            orientation = cursor.getInt(0);
+        }
+        cursor.close();
+        return orientation;
+    }
+
+    private Drawable getRotateDrawable(final Bitmap b, final float angle) {
+        final BitmapDrawable drawable = new BitmapDrawable(getResources(), b) {
+            @Override
+            public void draw(final Canvas canvas) {
+                canvas.save();
+                canvas.rotate(angle, b.getWidth() / 2, b.getHeight() / 2);
+                super.draw(canvas);
+                canvas.restore();
+            }
+        };
+        return drawable;
     }
 
 }
